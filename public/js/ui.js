24e12740b9019fd0b61dbdf578520e3a83b8157b
albertdeng <albertdeng@kkbox.com>
2018-07-27 14:35:29 +0800

    dropdown function

--- public/js/ui.js
+++ public/js/ui.js
@@ -10,7 +10,23 @@ $('.switch').click(function(){
 
 
 $('.alert').click(function(){
-  $('.alert_message').toggleClass( "alert_message_show" )
+  $('.alert_message').toggleClass( "show_show" )
+  return false
+});
+
+
+$('.font_size').click(function(){
+  $('.font_size_message').toggleClass( "show_show" )
+  return false
+});
+
+$('.fun_icons > .fun_edit').click(function(){
+  $('.edit_dropdown').toggleClass( "show_show" )
+  return false
+});
+
+$('.main_fun_edit').click(function(){
+  $('.main_edit_dropdown').toggleClass( "show_show" )
   return false
 });
 
